d58ec5bab45c6c25b7e5329ee431c80511d7edb9
Aditya Krishna Gupta <aditya@clueso.io>
2024-08-28 13:27:30 +0530

    changed scripts

--- ts-auth-service/main.tf
+++ ts-auth-service/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # Create an EC2 instance where the microservice will be deployed
 resource "aws_instance" "web" {
   ami           = "ami-0522ab6e1ddcc7055"  # Provided Ubuntu AMI
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name      = "Terraform_Cloudformation_Benchmarking"  # Provided key pair
 
   # Use the provided security group and subnet
@@ -18,4 +18,20 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Terraform-EC2-${basename(abspath(path.module))}"  # Unique name based on the microservice directory name
   }
+}
+
+# Create a new 10GB EBS volume
+resource "aws_ebs_volume" "ebs_volume" {
+  availability_zone = aws_instance.web.availability_zone
+  size              = 10
+  tags = {
+    Name = "Terraform-EBSSVolume-${basename(abspath(path.module))}"  # Unique name for the EBS volume
+  }
+}
+
+# Attach the EBS volume to the EC2 instance
+resource "aws_volume_attachment" "ebs_attachment" {
+  device_name = "/dev/xvdf"
+  volume_id   = aws_ebs_volume.ebs_volume.id
+  instance_id = aws_instance.web.id
 }
