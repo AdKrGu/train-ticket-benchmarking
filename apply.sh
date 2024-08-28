7f3c8582018bd83276f2b034f644d2777a3dc87f
Aditya Krishna Gupta <aditya@clueso.io>
2024-08-28 16:42:18 +0530

    changed scripts

--- apply.sh
+++ apply.sh
@@ -2,19 +2,25 @@
 
 BASE_DIR="/home/ubuntu/train-ticket-benchmarking"
 
-for dir in "$BASE_DIR"/ts-*/; do
+# Function to apply Terraform configuration for a given directory
+apply_terraform() {
+  local dir=$1
   echo "Applying $dir..."
+  cd "$dir" || { echo "Failed to navigate to $dir"; exit 1; }
+  terraform apply -auto-approve
+  echo "Completed Applying $dir"
+}
 
+# Iterate over each directory inside the base directory with prefix "ts-"
+for dir in "$BASE_DIR"/ts-*/; do
   if [ -f "$dir/main.tf" ]; then
-
-    cd "$dir" || { echo "Failed to navigate to $dir"; exit 1; }
-
-    terraform apply plan.tfplan
-    
-    echo "Completed Applying $dir"
+    apply_terraform "$dir" &
   else
     echo "Skipping $dir as main.tf is not present."
   fi
-
-  cd "$BASE_DIR" || exit
 done
+
+# Wait for all background jobs to finish
+wait
+
+echo "All terraform apply operations are complete."
--- init.sh
+++ init.sh
@@ -2,18 +2,25 @@
 
 BASE_DIR="/home/ubuntu/train-ticket-benchmarking"
 
-for dir in "$BASE_DIR"/ts-*/; do
+# Function to initialize Terraform for a given directory
+init_terraform() {
+  local dir=$1
   echo "Init $dir..."
+  cd "$dir" || { echo "Failed to navigate to $dir"; exit 1; }
+  terraform init -input=false
+  echo "Completed Init $dir"
+}
 
+# Iterate over each directory inside the base directory with prefix "ts-"
+for dir in "$BASE_DIR"/ts-*/; do
   if [ -f "$dir/main.tf" ]; then
-    cd "$dir" || { echo "Failed to navigate to $dir"; exit 1; }
-
-    terraform init -input=false
-    
-    echo "Completed Init $dir"
+    init_terraform "$dir" &
   else
     echo "Skipping $dir as main.tf is not present."
   fi
-
-  cd "$BASE_DIR" || exit
 done
+
+# Wait for all background jobs to finish
+wait
+
+echo "All terraform init operations are complete."
